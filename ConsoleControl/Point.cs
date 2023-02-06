ec65f478b6da2f9675fa5ed4c57bc61148017631
kulyai <kulyai@KOMPUTER>
2023-02-06 09:15:46 +0300

    deviding into files

---
+++ ConsoleControl/Point.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleControl
+{
+    enum Direction
+	{
+		right,
+		left,
+		up,
+		down,
+	}
+
+    class Point
+    {
+        public const int BORDER_BOTTOM = 10;
+        public const int BORDER_RIGHT = 25;
+
+        private int posX;
+        private int posY;
+
+        public int PosX
+        {
+            get => posX;
+            set
+            {
+                if (value < 0 || value >= BORDER_RIGHT)
+                {
+                    throw new Exception("Out of border");
+                }
+                posX = value;
+            }
+        }
+        public int PosY
+        {
+            get => posY;
+            set
+            {
+                if (value < 0 || value >= BORDER_BOTTOM)
+                {
+                    throw new Exception("Out of border");
+                }
+                posY = value;
+            }
+        }
+
+        public Point(Point p)
+        {
+            PosX = p.PosX;
+            PosY = p.PosY;
+        }
+        public Point(int x = 0, int y = 0)
+        {
+            PosX = x;
+            PosY = y;
+        }
+
+        public void MovePoint(Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.right:
+                    PosX += 1;
+                    break;
+                case Direction.left:
+                    PosX -= 1;
+                    break;
+                case Direction.up:
+                    PosY -= 1;
+                    break;
+                case Direction.down:
+                    PosY += 1;
+                    break;
+                default:
+                    break;
+            }
+        }
+        public void Refresh()
+        {
+            Console.Clear();
+            for (int i = 0; i < PosY; i++)
+            {
+                Console.WriteLine();
+            }
+            for (int i = 0; i < PosX; i++)
+            {
+                Console.Write(' ');
+            }
+            Console.Write('*');
+            Console.WriteLine();
+            for (int i = 0; i < BORDER_BOTTOM - PosY; i++)
+            {
+                Console.WriteLine();
+            }
+            Console.WriteLine($"\nPosX: {PosX}; PosY {BORDER_BOTTOM - PosY}");
+        }
+
+        public override bool Equals(object obj)
+        {
+            return (obj as Point)?.PosX == PosX
+                && (obj as Point)?.PosY == PosY;
+        }
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+        public override string ToString()
+        {
+            return $"X: {PosX}; Y: {PosY}";
+        }
+    }
+}
---
+++ ConsoleControl/Snake.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleControl
+{
+    class Snake
+    {
+        private List<Point> points = new List<Point>() { new Point() };
+        private static Random rnd = new Random();
+        private Point randomPoint = new Point()
+        {
+            PosX = rnd.Next(Point.BORDER_RIGHT - 1),
+            PosY = rnd.Next(Point.BORDER_BOTTOM - 1)
+        };
+
+        public void Add(Point p)
+        {
+            points.Add(p);
+        }
+        public void Refresh()
+        {
+            Console.Clear();
+            for (int i = 0; i <= Point.BORDER_BOTTOM; i++)
+            {
+                for (int j = 0; j <= Point.BORDER_RIGHT; j++)
+                {
+                    if (j == Point.BORDER_RIGHT || i == Point.BORDER_BOTTOM)
+                    {
+
+                        if (j == Point.BORDER_RIGHT)
+                        {
+                            Console.Write('|');
+                        }
+                        if (i == Point.BORDER_BOTTOM)
+                        {
+                            Console.Write('-');
+                        }
+                    }
+                    else
+                    {
+                        Point temp = new Point(j, i);
+                        if (points.Contains(temp) || temp.Equals(randomPoint))
+                        {
+                            Console.Write('*');
+                        }
+                        else
+                        {
+                            Console.Write(' ');
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+
+        }
+        public void Move(Direction direction)
+        {
+            for (int i = points.Count - 1; i > 0; i--)
+            {
+                Point current = points[i];// делаем, чтобы не производить обращение 2 раза
+                Point next = points[i - 1];
+                current.PosX = next.PosX;
+                current.PosY = next.PosY;
+            }
+
+            Point head = points[0];
+            head.MovePoint(direction);
+            if (!IsSet())
+            {
+                throw new Exception("End of game");
+            }
+            if (head.Equals(randomPoint))
+            {
+
+                Point newP = new Point(randomPoint);
+                points.Add(newP);
+                randomPoint = new Point(rnd.Next(Point.BORDER_RIGHT), rnd.Next(Point.BORDER_BOTTOM));
+            }
+
+        }
+
+        private bool IsSet()
+        {
+            var groups = points.GroupBy(g => g);
+            foreach (var group in groups)
+            {
+                if (group.Count() > 1) return false;
+            }
+            return true;
+        }
+
+
+    }
+}
